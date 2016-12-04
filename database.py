e2402681de37a233f6e0ccba2771a3b731585784
Piankov <piankov5206@gmail.com>
2016-12-04 23:21:43 +0300

    Добавил обработку кнопок, небольшой рефакторинг

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import sys
+print "Encoding is", sys.stdin.encoding
 
+time_list = [0, '15 минут', 'час', 'день']
+location_list = [0, 'дом', 'дача', 'работа', 'комп']
 
 def save_task(user, task_text):
     print "Save for %s this task: %s" % (user['username'], task_text)
@@ -7,5 +11,20 @@ def save_task(user, task_text):
     return id
     
     
+def update_task(user, id, location = 0, time = 0):
+    if time:
+        print "Change time for user %s, task %d, to %s" % (user, id, get_time(time))
+    if location:
+        print "Change location for user %s, task %d, to %s" % (user, id, get_location(location))
+ 
+ 
 def get_task(username, type, time):
     return "Go fuck yourself"
+    
+
+    
+def get_time(n):
+    return time_list[n]
+    
+def get_location(n):
+    return location_list[n]
--- response_methods.py
+++ response_methods.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from database import save_task, get_task
+from database import save_task, get_task, update_task, get_time, get_location
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import ast
 
 
 def start(bot, update):
@@ -9,22 +10,41 @@ def start(bot, update):
 
 def get_task(bot, update):
     id = save_task(update.message.from_user, update.message.text)
-    keyboard = [[InlineKeyboardButton("15 мин", callback_data='{id:%s, time:%s}' % (id, 1)),
-                 InlineKeyboardButton("час", callback_data='{id:%s, time:%s}' % (id, 2)),
-                 InlineKeyboardButton("день", callback_data='{id:%s, time:%s}' % (id, 3))],
+    def create_time_button(num):
+        return InlineKeyboardButton(get_time(num), callback_data="{'id':%s, 'time':%s}" % (id, num))
+    keyboard = [[create_time_button(i) for i in range(1, 4)]]
 
-                [InlineKeyboardButton("Дом", callback_data='3')]]
+                #[InlineKeyboardButton("Дом", callback_data='3')]]
                 
     reply_markup = InlineKeyboardMarkup(keyboard)
                 
     bot.sendMessage(chat_id=update.message.chat_id, text="Как думаешь, сколько времени это займёт?", reply_markup=reply_markup)
     
     
+    
 def button(bot, update):
     query = update.callback_query
-
-    bot.editMessageText(text="Selected option: %s" % query.data,
+    
+    
+    reply = ast.literal_eval(query.data)
+    if 'time' in reply:
+        bot.editMessageText(text="Ок, %s" % get_time(reply['time']),
                         chat_id=query.message.chat_id,
                         message_id=query.message.message_id)
+        update_task(query.message.from_user, reply['id'], time=reply['time'])
+                        
+        def create_location_button(num):
+            return InlineKeyboardButton(get_location(num), callback_data="{'id':%s, 'location':%s}" % (reply['id'], num))
+            
+        keyboard = [[create_location_button(i) for i in range(1, 5)]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+            
+        bot.sendMessage(chat_id=query.message.chat_id, text="Это где?", reply_markup=reply_markup)
+        
+    if 'location' in reply:
+        bot.editMessageText(text="Ок, %s" % get_location(reply['location']),
+                        chat_id=query.message.chat_id,
+                        message_id=query.message.message_id)
+        update_task(query.message.from_user, reply['id'], location=reply['location'])
     
     
