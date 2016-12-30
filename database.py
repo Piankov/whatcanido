35b5b529fb4850fc6a80ef3cc2b2d453638bc99c
root <root@piankov.haze.yandex.net>
2016-12-30 09:09:21 +0000

    Add updating time

--- database.py
+++ database.py
@@ -6,8 +6,8 @@ import MySQLdb.cursors
 
 print "Encoding is", sys.stdin.encoding
 
-time_list = [0, '15 минут', 'час', 'день']
-location_list = [0, 'дом', 'дача', 'работа', 'комп']
+time_list = [u'ВСЕГДА', u'15 минут', u'час', u'день']
+location_list = [u'ВЕЗДЕ', u'дом', u'дача', u'работа', u'комп']
 
 
 db = MySQLdb.connect (
@@ -76,18 +76,34 @@ def show_tasks(user):
 WHERE UserID = %d' % user_id
     cur.execute(query)
     tasks = parse_responce(cur)
-    tasks = [str(d) for d in tasks]
-    tasks = '\n'.join(tasks)
+    # tasks = [str(d) for d in tasks]
+    str_tasks = []
+    for d in tasks:
+        print d
+        tmp_task = '%s\n Time: %s, Location: %s' % (d['Description'], time_list[d['Time']], location_list[d['Location']])
+        if d['Favorite']:
+            tmp_task = '* ' + tmp_task
+            str_tasks = [tmp_task] + str_tasks
+        else:
+            str_tasks.append(tmp_task)
+    tasks = '\n'.join(str_tasks)
     return tasks
 
     
     
 def update_task(user, id, location = 0, time = 0):
+    user_id = get_user_id(user)
+    update_str = ''
     if time:
-        print "Change time for user %s, task %d, to %s" % (user, id, get_time(time))
+        update_str = 'Time = %d' % time
+        print "Change time for user %s, task %d, to %s" % (user['username'], id, get_time(time))
     if location:
-        print "Change location for user %s, task %d, to %s" % (user, id, get_location(location))
- 
+        update_str = 'Location = %d' % location
+        print "Change location for user %s, task %d, to %s" % (user['username'], id, get_location(location))
+    query = "UPDATE Tasks SET %s WHERE ID = %d AND UserID = %d" % (update_str, id, user_id); 
+    print "DATABASE!", query
+    cur.execute(query)
+    db.commit() 
  
 def get_task(username, type, time):
     return "Go fuck yourself"
